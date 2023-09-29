94ae5641b34260eca429a03e4a0a99d22961313d
Mac <ajeemmanuel221@gmail.com>
2023-09-29 17:03:07 +0100

    further development

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from "./components/js/Navbar"
 import Testimonies from './components/js/Testimonies'
+import Body from './components/js/Body'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Navbar />
       <Testimonies />
+      <Body />
     </div>
   )
 }
--- vite-project/src/components/js/Body.tsx
+++ vite-project/src/components/js/Body.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import "../css/body.css"
 
 const Body = () => {
+
+   const first = Math.floor((Math.random() + 1) * 10)
+   const second = Math.floor(Math.random() * 21)
+   const third = Math.floor(Math.random() * 21)
+
+   if (first === 11) {
+      alert("you just won the jackpot")
+   }
+
   return (
     <div>
-      
+
+      <div className="whole">
+
+        <div className="main">
+
+            <div className="intro">
+              <h1>YOU PLAY</h1> <br />
+              <h1 className='we-pay'>WE PAY!</h1>
+            </div>
+
+        </div>
+
+        <div className="numbers">
+
+            <div className="num1">
+              <h1>{first}</h1>
+            </div>
+            <div className="num2">
+              <h1>{second}</h1>
+            </div>
+            <div className="num3">
+              <h1>{third}</h1>
+            </div>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
