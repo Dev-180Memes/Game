3e70ae502d63758257e0c3729140617ae57d4153
Mac <ajeemmanuel221@gmail.com>
2023-09-30 21:37:27 +0100

    further development

--- vite-project/src/components/js/Body.tsx
+++ vite-project/src/components/js/Body.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../css/body.css"
 
 const Body = () => {
@@ -8,6 +8,18 @@ const Body = () => {
   const first = Math.floor((Math.random() + 1) * 10)
   const second = Math.floor(Math.random() * 21)
   const third = Math.floor(Math.random() * 21)
+
+  const [firstNumber, setFirstNumber] = useState()
+   
+  const setFirst = () => {
+    const first = Math.floor((Math.random() + 1) * 10)
+    setFirstNumber(first)
+  }
+
+  const general = () => {
+    setFirst();
+    check();
+  }
 
   //////////////////////////////////////////////////////
 
@@ -28,9 +40,17 @@ const Body = () => {
 
   //////////////////////////////////////////////////////
 
-  // if (first === 11) {
-  //   alert("you just won the jackpot")
-  // }
+  // the function to check if the numbers aligns////////
+
+  const check = () => {
+    if ( a === first && b === second ) {
+      alert("you're done")
+    } else {
+      alert("Oops! Try again")
+    }
+  }
+
+  //////////////////////////////////////////////////////
 
   return (
     <div>
@@ -49,7 +69,7 @@ const Body = () => {
         <div className="numbers">
 
             <div className="num1">
-              <h1>{first}</h1>
+              <h1>{firstNumber}</h1>
             </div>
             <div className="num2">
               <h1>{second}</h1>
@@ -69,18 +89,18 @@ const Body = () => {
                 <h2>POSSIBLE <br /> OUTCOME</h2>
               </div>
               <div className="outcome">
-                <p className='1'>{a}</p>
-                <p>{b}</p>
-                <p>{c}</p>
-                <p>{d}</p>
-                <p>{e}</p>
-                <p>{f}</p>
-                <p>{g}</p>
-                <p>{h}</p>
-                <p>{i}</p>
-                <p>{j}</p>
-                <p>{k}</p>
-                <p>{l}</p>
+                <p className='use1'>{a}</p>
+                <p className='use2'>{b}</p>
+                <p className='use3'>{c}</p>
+                <p className='use4'>{d}</p>
+                <p className='use5'>{e}</p>
+                <p className='use6'>{f}</p>
+                <p className='use7'>{g}</p>
+                <p className='use8'>{h}</p>
+                <p className='use9'>{i}</p>
+                <p className='use10'>{j}</p>
+                <p className='use11'>{k}</p>
+                <p className='use12'>{l}</p>
               </div>
             </div>
 
@@ -91,26 +111,26 @@ const Body = () => {
               <h2>SELECT YOUR<br />OUTCOMES</h2>
             </div>
             <div className="outcomes">
-              <p>1</p>
-              <p>2</p>
-              <p>3</p>
-              <p>4</p>
-              <p>5</p>
-              <p>6</p>
-              <p>7</p>
-              <p>8</p>
-              <p>9</p>
-              <p>10</p>
-              <p>11</p>
-              <p>12</p>
-              <p>13</p>
-              <p>14</p>
-              <p>15</p>
-              <p>16</p>
-              <p>17</p>
-              <p>18</p>
-              <p>19</p>
-              <p>20</p>
+              <p className='use13'>1</p>
+              <p className='use14'>2</p>
+              <p className='use15'>3</p>
+              <p className='use16'>4</p>
+              <p className='use17'>5</p>
+              <p className='use18'>6</p>
+              <p className='use19'>7</p>
+              <p className='use20'>8</p>
+              <p className='use21'>9</p>
+              <p className='use22'>10</p>
+              <p className='use23'>11</p>
+              <p className='use24'>12</p>
+              <p className='use25'>13</p>
+              <p className='use26'>14</p>
+              <p className='use27'>15</p>
+              <p className='use28'>16</p>
+              <p className='use29'>17</p>
+              <p className='use30'>18</p>
+              <p className='use31'>19</p>
+              <p className='use32'>20</p>
             </div>
           </div>
         </div>
@@ -120,29 +140,40 @@ const Body = () => {
       <div className="user-amount">
 
         <div className="bet-amount">
-          <form action="">
+          <form>
 
             <div className="the-amount">
 
               <div className="how-much">
                 <label htmlFor="amount-to-stake">HOW MUCH:</label>
 
-                <input type="number" required min="200" id='amount-to-stake' />
+                <input 
+                type="number" 
+                required min="200" 
+                id='amount-to-stake'
+                placeholder='Enter amount' 
+                />
               </div>
 
               <div className="multiplier">
 
                 <label htmlFor="multiplier">MULTIPLIER:</label>
 
-                <input type="number" id="multiplier" min="2" />
+                <input 
+                type="number" 
+                id="multiplier" 
+                min="2"
+                placeholder='Enter your multiplier' 
+                />
 
               </div>
 
             </div>
 
-            <button>PLAY!</button>
+            <button onClick={general} className='the-play'>PLAY!</button>
 
           </form>
+
         </div>
 
       </div>
